5cac930ba5e4f38b676004924fade586b1a9cb6e
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-03-22 16:59:05 -0700

    Moved index.html

--- assets/navbar/navbar_index.js
+++ assets/navbar/navbar_index.js
@@ -4,8 +4,8 @@ var navbar = `
 
     <div>
 
-        <a class="navbar-brand" href="../index.html">
-            <img id="tgo_logo" src="../assets/images/logo.png" alt="GTO Logo">
+        <a class="navbar-brand" href="index.html">
+            <img id="tgo_logo" src="assets/images/logo.png" alt="GTO Logo">
         </a>
         
         <a id="tgo_name" class="navbar-brand align-middle">THE GALACTIC ORDER</a>
@@ -34,8 +34,8 @@ var navbar = `
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown1">
-                    <li><a class="dropdown-item" href="../index.html#">Introduction</a></li>
-                    <li><a class="dropdown-item" href="../index.html#rules_laws_link">Rules & Laws</a></li>
+                    <li><a class="dropdown-item" href="index.html#">Introduction</a></li>
+                    <li><a class="dropdown-item" href="index.html#rules_laws_link">Rules & Laws</a></li>
                 </ul>
             </li>
 
@@ -46,9 +46,9 @@ var navbar = `
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown2">
-                    <li><a class="dropdown-item" href="../pages/2_rp_1.html">Protocol</a></li>
+                    <li><a class="dropdown-item" href="pages/2_rp_1.html">Protocol</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/2_rp_2.html">Officer Info</a></li>
+                    <li><a class="dropdown-item" href="pages/2_rp_2.html">Officer Info</a></li>
                 </ul>
             </li>
 
@@ -59,11 +59,11 @@ var navbar = `
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown3">
-                    <li><a class="dropdown-item" href="../pages/3_roster_1.html">Battalions</a></li>
+                    <li><a class="dropdown-item" href="pages/3_roster_1.html">Battalions</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/3_roster_2.html">Full Roster</a></li>
+                    <li><a class="dropdown-item" href="pages/3_roster_2.html">Full Roster</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/3_roster_3.html">Spec-Ops</a></li>
+                    <li><a class="dropdown-item" href="pages/3_roster_3.html">Spec-Ops</a></li>
                 </ul>
             </li>
 
@@ -74,13 +74,13 @@ var navbar = `
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown4">
-                    <li><a class="dropdown-item" href="../pages/4_lore_0.html">Founding</a></li>
-                    <li><a class="dropdown-item" href="../pages/4_lore_0.html#backstories_link">Backstories</a></li>
-                    <li><a class="dropdown-item" href="../pages/4_lore_0.html#goals_link">Goals</a></li>
+                    <li><a class="dropdown-item" href="pages/4_lore_0.html">Founding</a></li>
+                    <li><a class="dropdown-item" href="pages/4_lore_0.html#backstories_link">Backstories</a></li>
+                    <li><a class="dropdown-item" href="pages/4_lore_0.html#goals_link">Goals</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/4_lore_1.html">Classifications</a></li>
+                    <li><a class="dropdown-item" href="pages/4_lore_1.html">Classifications</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/4_lore_2.html">Combat Theory</a></li>
+                    <li><a class="dropdown-item" href="pages/4_lore_2.html">Combat Theory</a></li>
                 </ul>
             </li>
 
@@ -91,26 +91,26 @@ var navbar = `
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown5">
-                    <li><a class="dropdown-item" href="../pages/5_ranks_1.html">Rank Informtation</a></li>
+                    <li><a class="dropdown-item" href="pages/5_ranks_1.html">Rank Informtation</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/5_ranks_2.html">Rank Benefits</a></li>
+                    <li><a class="dropdown-item" href="pages/5_ranks_2.html">Rank Benefits</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/5_ranks_3.html">Captain Info</a></li>
+                    <li><a class="dropdown-item" href="pages/5_ranks_3.html">Captain Info</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../pages/5_ranks_4.html">Agent Info</a></li>
+                    <li><a class="dropdown-item" href="pages/5_ranks_4.html">Agent Info</a></li>
                 </ul>
             </li>
             
             <li class="nav-item">
-                <a id="navbar_calendar" class="nav-link" href="../pages/6_calendar.html"><i id="fa_nav_icon_square" class="fa-solid fa-calendar-days"></i>Calendar</a>
+                <a id="navbar_calendar" class="nav-link" href="pages/6_calendar.html"><i id="fa_nav_icon_square" class="fa-solid fa-calendar-days"></i>Calendar</a>
             </li>
 
             <li class="nav-item">
-                <a id="navbar_contact_us" class="nav-link" href="../pages/7_contact_us.html"><i id="fa_nav_icon_square" class="fa-solid fa-square-envelope"></i>Contact Us</a>
+                <a id="navbar_contact_us" class="nav-link" href="pages/7_contact_us.html"><i id="fa_nav_icon_square" class="fa-solid fa-square-envelope"></i>Contact Us</a>
             </li>
 
             <li class="nav-item">
-                <a id="navbar_resources" class="nav-link" href="../pages/8_resources.html"><i id="fa_nav_icon_round" class="fa-solid fa-circle-question"></i>Resources</a>
+                <a id="navbar_resources" class="nav-link" href="pages/8_resources.html"><i id="fa_nav_icon_round" class="fa-solid fa-circle-question"></i>Resources</a>
             </li>
 
         </ul>
