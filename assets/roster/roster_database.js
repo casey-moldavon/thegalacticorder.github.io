2e2b93fcf4e3049e4cfd6b85d0d869a91ed9480e
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-06-07 10:38:57 -0700

    Added science emblem

--- assets/roster/roster_database.js
+++ assets/roster/roster_database.js
@@ -1,7 +1,11 @@
 // unit_name (Star Citizen Name)
 // unit_contact (Discord Profile)
-// unit_recruiter (Recruiter's Name)
+// unit_recruiter [Recruiter's Name (optional)]
+// unit_battalion (Starting Battalion)
 // unit_start (Start Date)
+
+// Starting Battalion will be a string combining Fleet Name and Battalion Name (designation)
+
 
 // unit_rank_current (Current Rank) <-- not yet added (added automatically)
 // unit_promotion (Last Promotion Date) <-- not yet added (added automatically)
@@ -13,6 +17,9 @@
 // unit_hours (Added Hours)
 // unit_alerts (Issued Alerts)
 // unit_warning (Issued Warning)
+
+// on-click of issued warning, section opens with add date and text-area to write notes about the warning
+
 
 // mission_turret (Turret)
 // mission_ground_assault (Ground Assault)
@@ -27,4 +34,8 @@
 // mission_search_rescue (Search & Rescue)
 // mission_manage_data (Manage Data)
 // mission_research (Research)
+// mission_explore (Explore)
 // mission_agronomy (Agronomy)
+
+
+
