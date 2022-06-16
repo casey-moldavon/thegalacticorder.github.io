71f3a44408d3e572bf20cee453b15563e16f6f04
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-06-16 12:19:55 -0700

    add db query

--- server/netlify/functions/test.js
+++ server/netlify/functions/test.js
@@ -1,8 +1,29 @@
 
+const pg = require('pg');
+
 const handler = async (event) => {
+
+    var client = new pg.Client({
+        user: "hwfqfkgmawvess",
+        password: "ef75a216e6429794aa6ccd9f7ea654156770917cb4b0b76a1bb46a21acc882c1",
+        database: "da9o6t84ekkaj4",
+        port: 5432,
+        host: "ec2-52-72-99-110.compute-1.amazonaws.com",
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }); 
+
+    client.connect();
+
+
+    let res = await client.query(`
+        SELECT * FROM test_people
+    `);
+
     return {
         statusCode: 200,
-        body: event
+        body: JSON.stringify(res.rows)
     };
 }
 
