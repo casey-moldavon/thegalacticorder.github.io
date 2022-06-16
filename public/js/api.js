70d1b1a0c25312c7176f516869e22c4908180a50
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-06-16 16:44:26 -0700

    Updating files

--- public/js/api.js
+++ public/js/api.js
@@ -3,5 +3,10 @@ async function runTestEndpoint() {
     const response = await fetch('https://frolicking-frangipane-e2734e.netlify.app/.netlify/functions/test')
     const data = await response.json()
 
-    alert(JSON.stringify(data.rows));
+    let element = document.getElementById('people-placeholder');
+    let html = data.rows.map(row => `
+        <li><b>${row.name}</b>: ${row.number}</li>
+    `).join('');
+
+    element.innerHTML = html;
 }
