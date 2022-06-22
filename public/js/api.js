ffaaf54566a1ff9f5c793156fcc5beacbd4d8564
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-06-22 10:29:31 -0400

    Update server.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,15 +1,15 @@
 
-async function runTestEndpoint() {
-    const response = await fetch('https://frolicking-frangipane-e2734e.netlify.app/.netlify/functions/test')
-    const data = await response.json()
+// async function runTestEndpoint() {
+//     const response = await fetch('https://frolicking-frangipane-e2734e.netlify.app/.netlify/functions/test')
+//     const data = await response.json()
 
-    let element = document.getElementById('people-placeholder');
-    let html = data.rows.map(row => `
-        <li><b>${row.name}</b>: ${row.number}</li>
-    `).join('');
+//     let element = document.getElementById('people-placeholder');
+//     let html = data.rows.map(row => `
+//         <li><b>${row.name}</b>: ${row.number}</li>
+//     `).join('');
 
-    element.innerHTML = html;
-}
+//     element.innerHTML = html;
+// }
 
 
 
