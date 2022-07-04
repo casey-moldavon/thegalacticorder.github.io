f8d452ceb62e8163d171092232b28bf41af644ea
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-07-04 14:40:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ client.connect();
 
 
 client.query(`
-    SELECT * FROM full_roster ORDER BY unit_id ASC
+    SELECT * FROM full_roster ORDER BY UnitID ASC
 `).then(res => {
     console.log(res.rows)
 }).finally(() => client.end());
