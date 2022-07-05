814f2f3c8935d689d9c31bcfff6596d42775a926
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-07-05 06:55:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ var client = new pg.Client({
 client.connect();
 
 
-// client.query(`
-//     SELECT * FROM full_roster
-// `).then(res => {
-//     console.log(res.rows)
-// }).finally(() => client.end());
-
-
 client.query(`
-    SELECT * FROM full_roster ORDER BY unit_id ASC
+    SELECT * FROM full_roster
 `).then(res => {
     console.log(res.rows)
 }).finally(() => client.end());
+
+
+// client.query(`
+//     SELECT * FROM full_roster ORDER BY unit_id ASC
+// `).then(res => {
+//     console.log(res.rows)
+// }).finally(() => client.end());
