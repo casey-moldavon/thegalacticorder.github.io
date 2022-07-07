c826f8c021cd6140e8955a10229f165afb4d6895
casey-moldavon <57796447+casey-moldavon@users.noreply.github.com>
2022-07-07 12:43:07 -0700

    Updated roster ID styling

--- public/roster/roster_cards.js
+++ public/roster/roster_cards.js
@@ -258,22 +258,22 @@ async function generateCardData() {
         row =>
         `
         <div id="leadership_card" type="button" class="card text-center" data-bs-toggle="collapse" data-bs-target="#unit_id_${row.sc_name}" aria-expanded="false" aria-controls="unit_id_${row.sc_name}" style="width: 9rem;">
-        <div>
-        <img id="card_image" src="../../ranks/badges/5-admiral.png" class="card-img-top" alt="...">
-        </div>
-        <p id="card_text">${row.unit_role}</p>
-        <p id="card_name">${row.sc_name}</p>
+            <div>
+                <img id="card_image" src="../../ranks/badges/5-admiral.png" class="card-img-top" alt="...">
+            </div>
+            <p id="card_text">${row.unit_role}</p>
+            <p id="card_name">${row.sc_name}</p>
         </div>
 
         <div class="collapse fixed-top" id="unit_id_${row.sc_name}">
-            <div class="container container-fluid" id="unit_id_container">
+            <div class="container container-fluid" id="leadership_unit_id_container">
 
                 <div id="unit_id_close_button" type="button" data-bs-toggle="collapse" data-bs-target="#unit_id_${row.sc_name}" aria-expanded="false" aria-controls="unit_id_${row.sc_name}">
                     <i id="unit_id_close_button_icon" class="fa-solid fa-square-xmark"></i>
                 </div>
 
                 <div class="container">
-                    <img id="unit_id_image" class="img-fluid" src="../../ranks/badges/5-admiral.png" alt="Commanded Image">
+                    <img id="leadership_unit_id_image" class="img-fluid" src="../../ranks/badges/5-admiral.png" alt="Commanded Image">
                 </div>
 
                 <div id="leadership_name_container" class="container">
@@ -282,167 +282,173 @@ async function generateCardData() {
                     <p><a id="leadership_note">Rank ${row.unit_rank} : ${row.unit_role}</a></p>
                 </div>
 
-                <button type="button" data-bs-toggle="modal" data-bs-target="#unit_service_modal">
+                <button type="button" data-bs-toggle="collapse" data-bs-target="#unit_service_progression_collapse">
                     Service Progression
                 </button>
+                <button type="button" data-bs-toggle="collapse" data-bs-target="#unit_mission_progression_collapse">
+                    Mission Progression
+                </button>
+            </div>
+        </div>
 
 
-                <div class="modal fade" id="unit_service_modal" tabindex="-1" aria-hidden="true">
-                    <div class="modal-dialog modal-md">
-                        <div class="row justify-content-center text-center">
+        <div class="collapse fixed-top justify-content-center text-center" id="unit_service_progression_collapse">
+            <div class="container container-fluid">
+                <div class="row">
 
-                            <div class="col">
-                                <div id="stored_events">
-                                <p>Events</p>
-                                <p class="text-center" id="total_events">${row.events}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div id="stored_patrols">
-                                <p>Patrols</p>
-                                <p class="text-center" id="total_patrols">${row.patrols}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div id="stored_hours">
-                                <p>Hours</p>
-                                <p class="text-center" id="total_hours">${row.hours}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div id="stored_alerts">
-                                <p>Alerts</p>
-                                <p class="text-center" id="total_alerts">${row.alerts}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div id="stored_warnings">
-                                <p>Warnings</p>
-                                <p class="text-center" id="total_warnings">${row.warnings}</p>
-                                </div>
-                            </div>
+                    <div class="row">
+                        <div id="stored_events">
+                        <p>Events</p>
+                        <p class="text-center" id="total_events">${row.events}</p>
                         </div>
                     </div>
-                </div>
 
-                <div class="modal fade" id="unit_mission_modal" tabindex="-1" aria-hidden="true">
-                    <div class="row justify-content-center text-center">
-                        <div class="modal-dialog modal-md">
-
-                            <div class="col">
-                                <div type="button" id="stored_turret">
-                                <p>Turret</p>
-                                <p class="text-center" id="total_turret">${row.turret}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_ground_assault">
-                                <p>Ground Assault</p>
-                                <p class="text-center" id="total_ground_assault">${row.ground_assault}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_escort">
-                                <p>Escort</p>
-                                <p class="text-center" id="total_escort">${row.escort}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_inquiry">
-                                <p>Inquiry</p>
-                                <p class="text-center" id="total_inquiry">${row.inquiry}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_pilot_co_pilot">
-                                <p>Pilot / Co-Pilot</p>
-                                <p class="text-center" id="total_pilot_co_pilot">${row.pilot_co_pilot}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_mining">
-                                <p>Mining</p>
-                                <p class="text-center" id="total_mining">${row.mining}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_salvage">
-                                <p>Salvage</p>
-                                <p class="text-center" id="total_salvage">${row.salvage}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_cargo">
-                                <p>Cargo</p>
-                                <p class="text-center" id="total_cargo">${row.cargo}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_transit">
-                                <p>Transit</p>
-                                <p class="text-center" id="total_transit">${row.transit}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_support">
-                                <p>Support</p>
-                                <p class="text-center" id="total_support">${row.support}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_search_rescue">
-                                <p>Search & Rescue</p>
-                                <p class="text-center" id="total_search_rescue">${row.search_rescue}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_manage_data">
-                                <p>Manage Data</p>
-                                <p class="text-center" id="total_manage_data">${row.manage_data}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_research">
-                                <p>Research</p>
-                                <p class="text-center" id="total_research">${row.research}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_explore">
-                                <p>Explore</p>
-                                <p class="text-center" id="total_explore">${row.explore}</p>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <div type="button" id="stored_agronomy">
-                                <p>Agronomy</p>
-                                <p class="text-center" id="total_agronomy">${row.agronomy}</p>
-                                </div>
-                            </div>
-
+                    <div class="row">
+                        <div id="stored_patrols">
+                        <p>Patrols</p>
+                        <p class="text-center" id="total_patrols">${row.patrols}</p>
                         </div>
                     </div>
+
+                    <div class="row">
+                        <div id="stored_hours">
+                        <p>Hours</p>
+                        <p class="text-center" id="total_hours">${row.hours}</p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div id="stored_alerts">
+                        <p>Alerts</p>
+                        <p class="text-center" id="total_alerts">${row.alerts}</p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div id="stored_warnings">
+                        <p>Warnings</p>
+                        <p class="text-center" id="total_warnings">${row.warnings}</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
+
+
+        <div class="collapse fixed-bottom justify-content-center text-center" id="unit_mission_progression_collapse">
+            <div class="container container-fluid" id="unit_mission_progression_container">
+                <div class="row">
+
+                    <div class="col">
+                        <div id="stored_turret">
+                        <p>Turret</p>
+                        <p class="text-center" id="total_turret">${row.turret}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_ground_assault">
+                        <p>Ground Assault</p>
+                        <p class="text-center" id="total_ground_assault">${row.ground_assault}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_escort">
+                        <p>Escort</p>
+                        <p class="text-center" id="total_escort">${row.escort}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_inquiry">
+                        <p>Inquiry</p>
+                        <p class="text-center" id="total_inquiry">${row.inquiry}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_pilot_co_pilot">
+                        <p>Pilot / Co-Pilot</p>
+                        <p class="text-center" id="total_pilot_co_pilot">${row.pilot_co_pilot}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_mining">
+                        <p>Mining</p>
+                        <p class="text-center" id="total_mining">${row.mining}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_salvage">
+                        <p>Salvage</p>
+                        <p class="text-center" id="total_salvage">${row.salvage}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_cargo">
+                        <p>Cargo</p>
+                        <p class="text-center" id="total_cargo">${row.cargo}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_transit">
+                        <p>Transit</p>
+                        <p class="text-center" id="total_transit">${row.transit}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_support">
+                        <p>Support</p>
+                        <p class="text-center" id="total_support">${row.support}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_search_rescue">
+                        <p>Search & Rescue</p>
+                        <p class="text-center" id="total_search_rescue">${row.search_rescue}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_manage_data">
+                        <p>Manage Data</p>
+                        <p class="text-center" id="total_manage_data">${row.manage_data}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_research">
+                        <p>Research</p>
+                        <p class="text-center" id="total_research">${row.research}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_explore">
+                        <p>Explore</p>
+                        <p class="text-center" id="total_explore">${row.explore}</p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div id="stored_agronomy">
+                        <p>Agronomy</p>
+                        <p class="text-center" id="total_agronomy">${row.agronomy}</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     `).join('');
 
     novitiatePlaceholder.innerHTML = novitiateCards;
